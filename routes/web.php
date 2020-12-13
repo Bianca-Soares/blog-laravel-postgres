b3c0357729fa56cd7ea4eb8365a733e7e89c54ad
bianca-soares <biancasoaresdasilva2016@gmail.com>
2020-12-13 15:53:54 -0300

    add atleta

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AtletaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/home', function () {
+    return view('home');
+});
+
+
+Route::get('atleta/lista', [AtletaController::class, 'lista']);
+
+//Rotas para criar novo atleta
+Route::get('atleta/novo', [AtletaController::class, 'novo']);
+
+Route::post('atleta/inserir', [AtletaController::class, 'inserir']);
+
+
+Route::get('users', function (){
+    return "usuario ";
+});
+
+Route::get('users/{id}', function ($id) {
+    return "user id $id";
+});
+
+
